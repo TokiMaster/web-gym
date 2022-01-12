a62515362ef2d95e34c9c702d0e1c54491ff3780
unknown <popovictodor01@gmail.com>
2022-01-12 18:25:30 +0100

    Inicijalni commit

---
+++ src/main/java/home/Todor/SOVEuprava/Sr232020OwpApplication.java
@@ -0,0 +1,13 @@
+package home.Todor.SOVEuprava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sr232020OwpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sr232020OwpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/Todor/SOVEuprava/Sr232020OwpApplicationTests.java
@@ -0,0 +1,13 @@
+package home.Todor.SOVEuprava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sr232020OwpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
